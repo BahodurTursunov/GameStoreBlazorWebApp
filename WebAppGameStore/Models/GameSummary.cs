8f99f40d977d7009f4a3b9f0a9f99cb85fb79010
Турсунов Баходур <tursunovb18@gmail.com>
2024-05-29 20:46:47 +0500

    add webapp project with some changes

---
+++ WebAppGameStore/Models/GameSummary.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAppGameStore.Models
+{
+    public class GameSummary
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+
+    }
+}
